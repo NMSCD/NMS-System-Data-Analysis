1345031cf0437ee902fc9b2de1e11162d282d915
Lenni009 <guentherhanslphdyt@gmail.com>
2024-01-23 20:24:42 +0100

    update regions

--- main.ts
+++ main.ts
@@ -25,7 +25,7 @@ interface SystemData {
 	Platform: string;
 	Timestamp: string;
 	UnixTimestamp?: number;
-	'Correctly Tagged'?: boolean;
+	'Correctly Prefixed'?: boolean;
 }
 
 interface RegionData {
@@ -98,7 +98,7 @@ function extractData(obj: SystemDataRaw): SystemData {
 	if (validCivs.includes(civ.toLowerCase())) {
 		const { Glyphs, Name } = outputObj;
 		const tagStatus = isCorrectlyTagged(Glyphs, Name);
-		outputObj['Correctly Tagged'] = tagStatus;
+		outputObj['Correctly Prefixed'] = tagStatus;
 	}
 
 	return outputObj;
@@ -152,23 +152,23 @@ function buildExpectedTag(glyphs: string): string {
 	})();
 	const region = glyphs.substring(4);		// NoSonar this gets the region glyphs
 	const regionIndex = Object.keys(regionData[civ.toLowerCase()]).indexOf(region) + 1;
-	const expected = `[HUB${regionIndex}-${SIV}]`;
+	const expected = `EV${regionIndex}-${SIV}`;
 	return expected;
 }
 
 function createRegionTxt(regionName: string, regionDataArray: SystemData[]): void {
-	const headers = ['Other Systems', 'Missing HUB Tag'].map(heading => `==${heading}==`);
+	const headers = ['Other Systems', 'Missing Prefix'].map(heading => `==${heading}==`);
 	const other: string[] = [];
 	const missing: string[] = [];
 	const arrayCollection = [other, missing];
 	const txtContent: string[] = [];
 
 	for (const system of regionDataArray) {
-		if (system['Correctly Tagged']) {
+		if (system['Correctly Prefixed']) {
 			other.push(system.Name);
 		} else {
 			const expected = buildExpectedTag(system.Glyphs);
-			if (system.Name) missing.push(`${system.Name} (Expected Tag: ${expected.substring(1, expected.length - 1)})`);
+			if (system.Name) missing.push(`${system.Name} (Expected Prefix: ${expected.substring(1, expected.length - 1)})`);
 		}
 	}
 
@@ -189,60 +189,14 @@ function createJSON(filepath: string, dataArray: SystemData[]): void {
 
 function getRegionData(): RegionData {
 	return {
-		eishub: {
-			'F9556C30': 'Riwala',
-			'F9555C30': 'Omskio Instability',
-			'F9555C31': 'Marcki',
-			'F9556C31': 'Anolamga Spur',
-			'F9557C31': 'Sea of Zonyayp',
-			'F9557C30': 'Rayuyar Band',
-			'F9557C2F': 'Umaton Instability',
-			'F9556C2F': 'Exramb Adjunct',
-			'F9555C2F': 'Ologowe Fringe',
-			'FA556C30': 'Yatrifex',
-			'FA555C30': 'Yeiada Sector',
-			'FA555C31': 'Iptrevs Fringe',
-			'FA556C31': 'Yamiraith Sector',
-			'FA557C31': 'Wedragi Spur',
-			'FA557C30': 'Rezhensk',
-			'FA557C2F': 'Sobert Cloud',
-			'FA556C2F': 'Umtats Anomaly',
-			'FA555C2F': 'Tavill',
-			'F8556C30': 'Qangew Expanse',
-			'F8555C30': 'Nijhwal Boundary',
-			'F8555C31': 'Usband Cluster',
-			'F8556C31': 'Ejongaa Anomaly',
-			'F8557C31': 'Zahrel Expanse',
-			'F8557C30': 'The Arm of Fupand',
-			'F8557C2F': 'Cuculta',
-			'F8556C2F': 'Etmarao',
-			'F8555C2F': 'Otreie Void'
+		eisvana: {
+			'A21117FF': 'Flinus',
+			'A11117FF': 'Imnito',
+			'A11127FF': 'Hiuccli',
+			'A21127FF': 'Insesu',
+			'A21107FF': 'Cuynteto',
+			'A21107FE': 'Uskabar',
+			'A21117FE': 'Ziwananau',
 		},
-		calhub: {
-			'F9556C30': 'Uisaor Spur',
-			'F9555C30': 'The Arm of Kiffeyn',
-			'F9555C31': 'Ilongl Cloud',
-			'F9556C31': 'The Arm of Taticale',
-			'F9557C31': 'Egerap Anomaly',
-			'F9557C30': 'Wakestones Expanse',
-			'F9557C2F': 'Erhahn Fringe',
-			'F9556C2F': 'Imrikians Terminus',
-			'F9555C2F': 'Imedeili',
-			'FA556C30': 'Kovasu Adjunct',
-			'F8556C30': 'Lossians Boundary'
-		},
-		ghub: {
-			'F9556C30': 'The Arm of Vezitinen',
-			'F9555C30': 'Canthian',
-			'F9555C31': 'Dexterf Sector',
-			'F9556C31': 'The Arm of Katteus',
-			'F9557C31': 'Nugsdor Adjunct',
-			'F9557C30': 'Uefert Nebula',
-			'F9557C2F': 'Widraik',
-			'F9556C2F': 'Airnaka Conflux',
-			'F9555C2F': 'Sivess Instability',
-			'FA556C30': 'Savenix Instability',
-			'F8556C30': 'Nonlopsi Instability'
-		}
 	}
 }
